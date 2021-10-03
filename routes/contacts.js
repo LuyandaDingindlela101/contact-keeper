5549eb71a3355cdfb21dc30d041381963771d8b6
Luyanda <LuyandaDingindlela@gmail.com>
2021-10-03 22:15:44 +0200

    completed update contact route, now working on delete contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -60,8 +60,39 @@ router.post("/",
 )
 
 // ROUTE TO UPDATE A CONTACT BASED ON THE id
-router.put("/:id", auth, (request, response) => {
-    response.json("put contact");
+router.put("/:id", auth, async (request, response) => {
+    let contact_fields = {};
+    let { name, email, phone_number, type } = request.body;
+
+    // BUILD THE contact_fields OBJECT
+    if (name) contact_fields.name = name;
+    if (type) contact_fields.type = type;
+    if (email) contact_fields.email = email;
+    if (phone_number) contact_fields.phone_number = phone_number;
+
+    try {
+        // GET THE contact FROM THE DATABASE WITH THE PROVIDED id
+        let contact = await Contact.findById(request.params.id);
+        if (!contact) return response.status(404).json({ msg: "contact not found" });
+
+        // CHECK IF THE USER OWNS THE CONTACT THAT THEY WANNA UPDATE
+        if (contact.user.toString() !== request.user.id) return response.status(404).json({ msg: "user not authorised to update" });
+
+        contact = await Contact.findByIdAndUpdate(
+            // PROVIDE id OF CONTACT TO UPDATE
+            request.params.id, 
+            // PROVIDE THE FIELDS TO UPDATE
+            { $set: contact_fields },
+            // IF THE CONTACT DOES NOT EXIST, CREATE IT
+            { new: true }
+        );
+
+        response.json(contact);
+    } catch (error) {
+        
+        console.error(error.message);
+        response.status(500).send("Server error")
+    }
 })
 
 // ROUTE DELETE A CONTACT BASED ON THE id
